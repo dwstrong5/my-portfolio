8cba46c5e41345620befe472749e378126d575fa
Don Strong <80288957+dwstrong5@users.noreply.github.com>
2021-11-15 17:53:58 -0800

    Added initial components to app for navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Navbar, NavbarBrand } from 'react-bootstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Don Strong',
+      headerLinks: [
+        {
+          title: 'Home', 
+          path: '/',
+        },
+        {
+          title: 'About', 
+          path: '/about',
+        },
+        {
+          title: 'Contact', 
+          path: '/contact',
+        }
+      ],
+      home: {
+        title: 'Be Relentless',
+        subTitle: 'Projects that make a difference',
+        blurb: 'Checkout these projects',
+      },
+      about: {
+        title: 'About Me',
+      },
+      contact: {
+        title: 'Let\'s Talk',
+      },
+    }
+  }
+
+  render() {
+
+    return (
+
+      <Router>
+        <Container className = "p-0" fluid = {true}>
+
+          <Navbar className="border-bottom">
+            <Navbar.Brand>Don Strong</Navbar.Brand>
+            
+          </Navbar>
+
+        </Container>
+      </Router>
+
+    );
+
+  }
+
+
 }
 
 export default App;
