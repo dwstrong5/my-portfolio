20292b17d6baddc10fd7925d63d361ee26e7f41f
Don Strong <80288957+dwstrong5@users.noreply.github.com>
2023-09-14 12:34:25 -0700

    added favicon, started work on contact page

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public/')));
+
+// Get MongoDB credentials and generate connection String
+fs.readFile('config.txt', (err, data) => {
+  if (err) throw err;
+  const creds = JSON.parse(data.toString());
+  const uri = `mongodb+srv://${creds.username}:${creds.password}@cluster0.2hj2thp.mongodb.net/${creds.database}?retryWrites=true&w=majority`;
+  // Connect to MongoDB using the connection string
+  mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  })
+  .then(() => {
+      console.log('Connected to MongoDB...');
+  })
+  .catch((error) => {
+      console.log("Error connecting to MongoDB: ", error);
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -14,6 +33,6 @@ app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, "/public/assets.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
